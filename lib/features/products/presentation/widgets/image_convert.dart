b9e63c62a6211ad236fe966d3666c6e43382ed23
admin <mohammedzewin01@gmail.com>
2025-02-26 01:27:24 +0300

    change ui

--- lib/features/products/presentation/widgets/image_convert.dart
+++ lib/features/products/presentation/widgets/image_convert.dart
@@ -1,37 +1,34 @@
-import 'dart:typed_data';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-Future<Uint8List> fetchImageBytes(String url) async {
-  final response = await http.get(Uri.parse(url));
-  if (response.statusCode == 200) {
-    return response.bodyBytes;
-  } else {
-    throw Exception('Failed to load image');
-  }
-}
-
-class ImageFromBytes extends StatelessWidget {
-  final String imageUrl;
-
-  const ImageFromBytes({super.key, required this.imageUrl});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<Uint8List>(
-      future: fetchImageBytes(imageUrl),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Image.asset(
-              'assets/images/image_default.jpg'); // صورة افتراضية أثناء التحميل
-        } else if (snapshot.hasError) {
-          return Image.asset('assets/images/image_default.jpg',
-              fit: BoxFit.fill); // صورة بديلة عند حدوث خطأ
-        } else {
-          return Image.memory(snapshot.data!, fit: BoxFit.cover);
-        }
-      },
-    );
-  }
-}
+// import 'dart:html' as html;
+// import 'dart:ui' as ui; // استيراد dart:ui
+// import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'package:flutter/material.dart';
+// import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+//
+// class WebImageWidget extends StatelessWidget {
+//   final String imageUrl;
+//
+//   const WebImageWidget({super.key, required this.imageUrl});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     if (!kIsWeb) {
+//       return SizedBox.shrink(); // لا تحتاج للعرض على الأجهزة غير الويب
+//     }
+//
+//     final String viewType = 'web-image-$imageUrl'; // استخدام imageUrl لجعل viewType فريدًا
+//
+//     // إنشاء عنصر الـ Image في الـ HTML
+//     final html.Element imgElement = html.ImageElement()
+//       ..src = imageUrl
+//       ..style.width = '100%'
+//       ..style.height = 'auto';
+//
+//     // ربط العنصر بالمشاهد
+//     ui..registerViewFactory(
+//       viewType,
+//           (int viewId) => imgElement,
+//     );
+//
+//     return HtmlElementView(viewType: viewType);
+//   }
+// }
